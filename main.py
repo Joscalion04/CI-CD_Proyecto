51b80cd28f4537e8c32848576becf90d23e19425
JeffersonSR <91024572+NanoCR24@users.noreply.github.com>
2024-05-14 21:24:47 -0600

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route("/") # Funcion para ruta predeterminada
 def Hello():
-    return "Hello, World!"
+    return "Hello, World! soy nano"
 
 app.run(host="0.0.0.0") #Host para Docker
